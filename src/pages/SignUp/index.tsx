ec1ac31e29bd5473d3e2757e237f363abc23810f
Rogerio Azevedo <rogerio.francis@gmail.com>
2020-10-31 12:21:30 -0400

    add signUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import logo from '../../assets/logo.svg'
 
 import Input from '../../components/Input'
@@ -7,15 +7,18 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   return (
     <Container>
+      <Background />
+
       <Content>
         <img src={logo} alt="GoBarber" />
 
         <form>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu cadastro</h1>
 
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
           <Input
@@ -25,19 +28,16 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </form>
 
         <a href="login">
-          <FiLogIn />
-          Criar uma conta
+          <FiArrowLeft />
+          Voltar para logon
         </a>
       </Content>
-      <Background />
     </Container>
   )
 }
 
-export default SignIn
+export default SignUp
