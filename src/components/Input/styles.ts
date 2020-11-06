4ee2ed3001be12d2978360136fc2a40be0e8e0cf
Rogerio Azevedo <rogerio.francis@gmail.com>
2020-11-06 11:21:08 -0400

    add tooltip do input

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components'
+import Tooltip from '../Tooltip'
 
 interface ContainerProps {
   isFocused: boolean
   isFilled: boolean
+  isErrored: boolean
 }
 export const Container = styled.div<ContainerProps>`
   background: #232129;
@@ -20,6 +22,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       color: #ff9000;
@@ -31,6 +39,8 @@ export const Container = styled.div<ContainerProps>`
     css`
       color: #ff9000;
     `}
+
+   
 
   input {
     background: transparent;
@@ -45,5 +55,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `
